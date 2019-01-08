8ba16b19791b9d3909792b3cd71fa5e1a92ed8a6
Dhiego Cassiano Fogaça Barbosa <modscleo4@outlook.com>
2019-01-08 03:18:47 -0200

    License updates
    Signed-off-by: Dhiego Cassiano Fogaça Barbosa <modscleo4@outlook.com>

--- client.js
+++ client.js
@@ -1,3 +1,23 @@
+/**
+ * Copyright 2019 Dhiego Casssiano Fogaça Barbosa
+
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+
+ * http://www.apache.org/licenses/LICENSE-2.0
+
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @file This is the main script of JSDB Client
+ *
+ * @author Dhiego Cassiano Fogaça Barbosa <modscleo4@outlook.com>
+ * */
+
 const net = require('net');
 
 const {
@@ -135,7 +155,7 @@ client.on('data', function (data) {
         }
 
         if (o['time'] !== 'NOTIME') {
-            t += o['time'];
+            t = o['time'];
         }
 
         if (t !== 0) {
